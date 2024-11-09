efbf5d3209b43832f660c09b29de6f077c21b649
Melendo <nachomelendo@gmail.com>
2024-11-09 12:44:53 +0100

    Configurado el servidor en lo mas básico para que funcione

---
+++ app.js
@@ -0,0 +1,24 @@
+"use strict";
+
+//Requires
+const express = require('express');
+const path = require('path');
+//const db = require('/dataBase/db');
+
+//Definimos el servidor y el puerto
+const app = express();
+const port = 3000;
+
+//Middleware
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({extended: true}));
+
+//Maneja peticion get al root
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+
+app.listen(port, () =>{
+    console.log('Servidor escuchando en http://localhost:' + port);
+});
