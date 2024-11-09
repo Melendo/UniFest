ac7de7a07d18f608c18f3124b5b6365d98edb2e0
Melendo <nachomelendo@gmail.com>
2024-11-09 12:57:10 +0100

    Actualización

---
+++ dataBase/db.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const mysql = require("mysql");
+
+const pool = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "AW_24"
+});
+
+// Función para ejecutar consultas a la base de datos
+function query(sql, params) {
+    return new Promise((resolve, reject) => {
+      pool.getConnection((err, connection) => {
+        if (err) {
+          reject(err);
+        } else {
+          connection.query(sql, params, (error, results) => {
+            connection.release(); // Libera la conexión una vez terminada la consulta
+            if (error) {
+              reject(error);
+            } else {
+              resolve(results);
+            }
+          });
+        }
+      });
+    });
+  }
+  
+  module.exports = { query };
