4627aae57cd596aa799afd00f65c38f07f98bf52
Oridori... <124064876+oridori02@users.noreply.github.com>
2024-11-14 12:49:59 +0100

    Estructura creada

--- app.js
+++ app.js
@@ -1,24 +1,51 @@
-"use strict";
+"use strict"
 
 //Requires
-const express = require('express');
-const path = require('path');
-const db = require('./dataBase/db');
+var express = require('express');
+var path = require('path');
 
-//Definimos el servidor y el puerto
-const app = express();
-const port = 3000;
+var createError = require('http-errors');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+//Definimos el servidor
+var app = express();
 
 //Middleware
-app.use(express.static(path.join(__dirname, "public")));
-app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
 
-//Maneja peticion get al root
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.use(express.json());
+app.use(cookieParser());
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-app.listen(port, () =>{
-    console.log('Servidor escuchando en http://localhost:' + port);
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
+
+module.exports = app;
