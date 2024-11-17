9789c51ccd48d3f658fd32b24bdd8e7b803c2145
Melendo <nachomelendo@gmail.com>
2024-11-17 21:40:43 +0100

    Todavia queda trabajo en el registro

--- routes/register.js
+++ routes/register.js
@@ -10,12 +10,7 @@ router.get('/', function(req, res, next) {
 
 router.post('/register', async (req, res) => {
   // Obtención de datos del formulario
-  const { nombre, telefono, email, contrasenia, confContrasenia, facultad, esOrganizador} = req.body;
-
-  // Validar que los campos no estén vacíos (si no lo has hecho ya en el frontend)
-  if (!nombre || !telefono || !email || !contrasenia || !confContrasenia || !facultad) {
-    return res.status(400).json({ message: 'Todos los campos son obligatorios.' });
-  }
+  const { nombre, telefono, email, contrasenia, confContrasenia, facultad, esOrganizador } = req.body;
 
   // Validación de contraseñas coincidentes
   if (contrasenia !== confContrasenia) {
@@ -33,8 +28,24 @@ router.post('/register', async (req, res) => {
     return res.status(400).json({ message: 'El correo debe ser un correo institucional de la UCM (ucm.es).' });
   }
 
-  // Hashear la contraseña antes de guardarla en la base de datos
   try {
+    // Comprobar que el correo no esté registrado
+    const QueryCorreoExiste = 'SELECT * FROM usuarios WHERE correo = ?';
+    const [resCorreo] = await db.query(QueryCorreoExiste, [email]);
+    if (resCorreo.length > 0) {
+      return res.status(400).json({ message: 'El correo ya está registrado.' });
+    }
+
+    // Comprobar que la facultad existe en la tabla facultades y conseguir su id para la petición
+    const QueryFacultadExiste = 'SELECT ID FROM facultades WHERE nombre = ?';
+    const [resFacultad] = await db.query(QueryFacultadExiste, [facultad]);
+    if (resFacultad.length === 0) {
+      return res.status(400).json({ message: 'La facultad no existe.' });
+    }
+    const idFacultad = facultadCheckResult[0].ID;
+
+    
+    // Hasheo de la contraseña antes de guardarla en la base de datos
     const hashedPassword = await bcrypt.hash(contrasenia, 10);
 
     // Consulta SQL para insertar los datos en la base de datos
@@ -42,13 +53,13 @@ router.post('/register', async (req, res) => {
       INSERT INTO usuarios (nombre, telefono, correo, ID_facultad, organizador, contrasenia)
       VALUES (?, ?, ?, ?, ?, ?)
     `;
-    const params = [nombre, telefono, email, 1, esOrganizador, hashedPassword];
+    const params = [nombre, telefono, email, idFacultad, esOrganizador, hashedPassword];
 
     // Ejecutar la consulta de inserción en la base de datos
     await db.query(query, params);
     
     // Si todo es exitoso, redirigir al usuario al login
-    res.status(200).json({message: 'Registro exitoso, por favor inicie sesión.'});
+    res.status(200).json({ message: 'Registro exitoso, por favor inicie sesión.' });
   } catch (error) {
     // Si ocurre un error al hacer el hash o la consulta, enviar un error 500
     console.error('Error al registrar el usuario:', error);
