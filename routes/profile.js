a6f5d385aefe2b3efe4071289121d5a8f815cde4
Oridori... <124064876+oridori02@users.noreply.github.com>
2024-11-23 16:20:24 +0100

    Carga dinamica datos usuario

--- routes/profile.js
+++ routes/profile.js
@@ -9,15 +9,15 @@ router.get('/', async (req, res) => {
     const query = 'SELECT * FROM usuarios WHERE id = ?';
     const [user] = await db.query(query, [16]);
     
-    const queryEventos = 'SELECT * FROM eventos WHERE ID_usuario = ?';
-    const [events] = [];
-    events = await db.query(queryEventos, [16]);
+    // const queryEventos = 'SELECT * FROM eventos WHERE ID_usuario = ?';
+    // const [events] = [];
+    // events = await db.query(queryEventos, [16]);
 
     // Verifica si el usuario existe
     if (!user) {
       return res.status(400).json({ message: 'Usuario no encontrado.' });
     }
-    else res.render('profile', {user: user, events: events});
+    else res.render('profile', {user: user});
   
   }catch(error){
     return res.status(500).json({ message: 'Hubo un error al procesar la solicitud. Intenta de nuevo.' });
