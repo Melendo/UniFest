034438156f696d48aaa611969c6e7cd5e0021921
Oridori... <124064876+oridori02@users.noreply.github.com>
2024-11-23 14:07:21 +0100

    implementando user en profile

--- routes/profile.js
+++ routes/profile.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../dataBase/db');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('profile', {rol:"usuario"});
+router.get('/', async (req, res) => {
+
+  try{
+    const query = 'SELECT * FROM usuarios WHERE id = ?';
+    const [user] = await db.query(query, [16]);
+    
+    const queryEventos = 'SELECT * FROM eventos WHERE ID_usuario = ?';
+    const [events] = [];
+    events = await db.query(queryEventos, [16]);
+
+    // Verifica si el usuario existe
+    if (!user) {
+      return res.status(400).json({ message: 'Usuario no encontrado.' });
+    }
+    else res.render('profile', {user: user, events: events});
+  
+  }catch(error){
+    return res.status(500).json({ message: 'Hubo un error al procesar la solicitud. Intenta de nuevo.' });
+  }
+
   
 });
 
