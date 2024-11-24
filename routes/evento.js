955fc3ef42bff8b53f5429ff3159756adc6d2d62
Melendo <nachomelendo@gmail.com>
2024-11-24 20:27:15 +0100

    Me estoy volviendo loco lo dejo por hoy

--- routes/evento.js
+++ routes/evento.js
@@ -4,6 +4,24 @@ var router = express.Router();
 var db = require('../dataBase/db');
 
 
+router.get('/misEventos', async(req, res) => {
+    // Verificar si el usuario está autenticado
+    if (!req.session.userId) {
+        return res.redirect('/login'); // Redirigir al login si no está autenticado
+    }
+    res.render('misEventos', { rol: req.session.rol });
+
+});
+
+router.get('/misInscripciones', async(req, res) => {
+    // Verificar si el usuario está autenticado
+    if (!req.session.userId) {
+        return res.redirect('/login'); // Redirigir al login si no está autenticado
+    }
+    res.render('misInscripciones', { rol: req.session.rol });
+
+});
+
 /* GET home page. */
 router.get('/:id', async(req, res) =>{
     
@@ -20,7 +38,7 @@ router.get('/:id', async(req, res) =>{
 
         const queryEvento = 'SELECT * FROM eventos WHERE ID = ?';
 
-        const [resEvento] = await db.query(queryEvento, idEvento);
+        const resEvento = await db.query(queryEvento, idEvento);
 
         if (!resEvento) {
             return res.status(400).json({ message: 'Evento no encontrado.' });
@@ -37,5 +55,8 @@ router.get('/:id', async(req, res) =>{
     
     
 });
+
+
+
 
 module.exports = router;
