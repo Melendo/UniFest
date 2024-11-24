539f3a3e4f066dfb265ad3e55839410f20957852
Melendo <nachomelendo@gmail.com>
2024-11-24 12:59:03 +0100

    Arreglados cosas del header/footer, logout terminado y trabajando en perfil

--- routes/profile.js
+++ routes/profile.js
@@ -4,10 +4,14 @@ var db = require('../dataBase/db');
 
 /* GET home page. */
 router.get('/', async (req, res) => {
+  // Verificar si el usuario está autenticado
+  if (!req.session.userId) {
+    return res.redirect('/login'); // Redirigir al login si no está autenticado
+}
 
   try{
     const query = 'SELECT * FROM usuarios WHERE id = ?';
-    const [user] = await db.query(query, [16]);
+    const [user] = await db.query(query, [req.session.userId]);
     
     // const queryEventos = 'SELECT * FROM eventos WHERE ID_usuario = ?';
     // const [events] = [];
@@ -17,7 +21,12 @@ router.get('/', async (req, res) => {
     if (!user) {
       return res.status(400).json({ message: 'Usuario no encontrado.' });
     }
-    else res.render('profile', {user: user});
+    else{
+      console.log(user.ID_facultad);
+      const query = 'SELECT nombre FROM facultades WHERE id = ?';
+      const [resfacultad] = await db.query(query, [user.ID_facultad]);
+      res.render('profile', {user: user, facultad: resfacultad});
+    }
   
   }catch(error){
     return res.status(500).json({ message: 'Hubo un error al procesar la solicitud. Intenta de nuevo.' });
