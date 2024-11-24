49ae6fde91b861f0a2089a0a615b54a852e2567b
Melendo <nachomelendo@gmail.com>
2024-11-24 16:46:48 +0100

    Perfil terminado excepto modificar datos de usuarios

--- routes/profile.js
+++ routes/profile.js
@@ -2,26 +2,47 @@ var express = require('express');
 var router = express.Router();
 var db = require('../dataBase/db');
 
+function formatearFecha(fecha) {
+  const fechaObj = new Date(fecha);
+  const dia = fechaObj.getDate().toString().padStart(2, '0'); // Día con 2 dígitos
+  const mes = (fechaObj.getMonth() + 1).toString().padStart(2, '0'); // Mes (enero es 0, así que sumamos 1)
+  const anio = fechaObj.getFullYear().toString().slice(-2); // Últimos 2 dígitos del año
+  return `${dia}/${mes}/${anio}`;
+}
+
 /* GET home page. */
 router.get('/', async (req, res) => {
+
   // Verificar si el usuario está autenticado
   if (!req.session.userId) {
-    return res.redirect('/login'); // Redirigir al login si no está autenticado
+    return res.redirect('/login'); 
 }
 
   try{
-    const query = 'SELECT * FROM usuarios WHERE id = ?';
-    const [user] = await db.query(query, [req.session.userId]);
+
+    //Consulta datos usuario
+    const queryUsuario = 'SELECT * FROM usuarios WHERE id = ?';
+    const [user] = await db.query(queryUsuario, [req.session.userId]);
 
     // Verifica si el usuario existe
     if (!user) {
       return res.status(400).json({ message: 'Usuario no encontrado.' });
     }
     else{
-      console.log(user.ID_facultad);
-      const query = 'SELECT nombre FROM facultades WHERE id = ?';
-      const [resfacultad] = await db.query(query, [user.ID_facultad]);
-      res.render('profile', {user: user, facultad: resfacultad, rol: req.session.rol});
+      //Consulta nombre facultad
+      const queryFacultad = 'SELECT nombre FROM facultades WHERE id = ?';
+      const [resfacultad] = await db.query(queryFacultad, [user.ID_facultad]);
+      
+
+      //Consulta historial de eventos
+      const queryEventosPasados = "SELECT eventos.título, eventos.fecha FROM eventos JOIN inscripciones ON eventos.ID = inscripciones.ID_evento WHERE inscripciones.ID_usuario = ? AND inscripciones.estado = 'inscrito' AND eventos.fecha < NOW() ORDER BY eventos.fecha DESC";
+      const resEventos = await db.query(queryEventosPasados, [req.session.userId]);
+
+      resEventos.forEach(evento =>{
+        evento.fecha = formatearFecha(evento.fecha);
+      })
+
+      res.render('profile', {user: user, facultad: resfacultad, rol: req.session.rol, historial: resEventos});
     }
   
   }catch(error){
