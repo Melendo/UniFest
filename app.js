879dff32af0f7ba48a648c8dc08b22c6852dbea5
Oridori... <124064876+oridori02@users.noreply.github.com>
2024-11-26 12:01:56 +0100

    app actualizado contra crasheo

--- app.js
+++ app.js
@@ -16,10 +16,8 @@ var profileRouter = require('./routes/profile');
 var homeRouter = require('./routes/home');
 var eventoRouter = require('./routes/evento');
 var misEventosRouter = require('./routes/misEventos');
-var misEventosRouter = require('./routes/notificaciones');
 
 var sessionMiddleware = require('./dataBase/session');
-var jobsMiddleware = require('./jobs/cron');
 
 //Definimos el servidor
 var app = express();
@@ -45,7 +43,6 @@ app.use('/profile', profileRouter);
 app.use('/home', homeRouter)
 app.use('/evento', eventoRouter);
 app.use('/misEventos', misEventosRouter);
-app.use('/notificaciones', notificacionesRouter);
 
 //Captura error 404 
 app.use(function(req, res, next) {
