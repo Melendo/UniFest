34d61b2236ab6b58ffa1f0e955061440e8f53031
Oridori... <124064876+oridori02@users.noreply.github.com>
2024-11-28 18:54:34 +0100

    al modal le caigo mal

--- public/javascripts/menuAccesibilidad.js
+++ public/javascripts/menuAccesibilidad.js
@@ -8,16 +8,52 @@ $(document).ready(function () {
     const esquema_colores = $("#esquema-colores").val();
 
     const tables = document.querySelectorAll("table");
+    const buttons = document.querySelectorAll("button");
+    const links = document.querySelectorAll("a");
+    const inputs = document.querySelectorAll("input");
+    const textareas = document.querySelectorAll("textarea");
 
     if (esquema_colores === "oscuro") {
       document.body.classList.add("dark-scheme");
-      tables.forEach((table) => {
+      tables.forEach(table => {
         table.classList.add("table-dark");
       });
     } else {
       document.body.classList.remove("dark-scheme");
       tables.forEach(table => {
         table.classList.remove("table-dark");
+      });
+    }
+
+    if (tamaño_letra === "grande") {
+      document.body.classList.remove("fs-4");
+      document.body.classList.add("fs-5");
+      buttons.forEach(button => {
+        button.classList.add("btn-lg");
+      });
+      links.forEach(link => {
+        link.classList.remove("fs-4");
+        link.classList.add("fs-5");
+      });
+    } else if (tamaño_letra === "muy-grande"){
+      document.body.classList.remove("fs-5");
+      document.body.classList.add("fs-4");
+      buttons.forEach(button => {
+        button.classList.add("btn-lg");
+      });
+      links.forEach(link => {
+        link.classList.remove("fs-5");
+        link.classList.add("fs-4");
+      });
+    }else {
+      document.body.classList.remove("fs-5");
+      document.body.classList.remove("fs-4");
+      buttons.forEach(button => {
+        button.classList.remove("btn-lg");
+      });
+      links.forEach(link => {
+        link.classList.remove("fs-4");
+        link.classList.remove("fs-5");
       });
     }
   });
